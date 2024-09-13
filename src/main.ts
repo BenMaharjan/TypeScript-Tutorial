3c86669bd12981b178404410635f46be9980594a
Ben Maharjan <benmaharjan@hotmail.co.uk>
2024-09-13 10:17:10 +0100

    first commit

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+let greeting:string="Hello! I am Typescript Tutor";
+let numOne:number=1;
+let numTwo:number=2;
+let trueOrFalse:boolean=true;
+
+
+function one(greeting) {
+  return greeting;
+}
+
+function two(numOne) {
+  return numOne;
+}
+
+function three(trueOrFalse) {
+  return trueOrFalse;
+}
+
+function four(numOne, numTwo) {
+  return numOne + numTwo;
+}
